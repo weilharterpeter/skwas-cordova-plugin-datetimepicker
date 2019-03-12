6a840e731b7190a0ff0b4a563c914e0caf6b1ae7
Peter Weilharter <EXTWeP@ammann-group.com>
2019-03-12 14:55:05 +0100

    fixes 1. jan problem on ios

--- src/ios/DateTimePicker.m
+++ src/ios/DateTimePicker.m
@@ -176,7 +176,6 @@
     datePicker.minuteInterval = minuteInterval;
 
     // Set to something else first, to force an update.
-    datePicker.date = [NSDate dateWithTimeIntervalSince1970:0];
     datePicker.date = [self getRoundedDate:[[NSDate alloc] initWithTimeIntervalSince1970:(ticks / DDBIntervalFactor)] minuteInterval:minuteInterval];
 }
 
